cf231be01473cd2e23db5d7c6902050ebd8a067c
Dan Amador <danamadorpe@gmail.com>
2018-10-20 14:57:29 +0200

    Basic movement finished

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public MovableCharacter characterOne, characterTwo;
+
+
+
+
+	private static PlayerController _instance;
+	public static PlayerController Instance { get { return _instance; } }
+
+
+	private void Awake() {
+		if (_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+		}
+		else {
+			_instance = this;
+		}
+	}
+	private void Update() {
+		// ReadInputManager.ReadAxes();
+		if (Input.GetButton("Horizontal1") || Input.GetButton("Vertical1")) {
+			characterOne.UpdatePosition(new Vector2(
+			Input.GetAxis("Horizontal1"),
+			Input.GetAxis("Vertical1")));
+		}
+
+		if (Input.GetButton("Horizontal2") || Input.GetButton("Vertical2")) {
+			Debug.Log("Aw shit boii");
+			characterTwo.UpdatePosition(new Vector2(
+			Input.GetAxis("Horizontal2"),
+			Input.GetAxis("Vertical2")));
+		}
+
+	}
+}
