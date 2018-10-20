eee38837bd69a62c9dd2e5175617d666698443cf
ALien747 <22078283+ALien747@users.noreply.github.com>
2018-10-20 15:21:03 +0200

    Chilli get eated

---
+++ Assets/Code/EatChilli.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatChilli : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.name);
+        Destroy(col);
+    }
+
+    //void OnTriggerEnter(Collision col)
+    //{
+    //    Debug.Log(col.gameObject.name);
+
+    //    if (col.gameObject.name == "Cube")
+    //    {
+    //        Destroy(col.gameObject);
+    //    }
+    //}
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
