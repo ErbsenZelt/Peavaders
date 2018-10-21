2a0dd73ea85a4021d135f48c745ecb33b7550aa5
Dan Amador <danamadorpe@gmail.com>
2018-10-21 14:59:52 +0200

    Added chili particles

--- Assets/Code/ChilliGetEeated.cs
+++ Assets/Code/ChilliGetEeated.cs
@@ -2,70 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChilliGetEeated : MonoBehaviour
-{
+public class ChilliGetEeated : MonoBehaviour {
 
 
-    //Counts Time for triggerd Damage Script - Every Second Damage should happen
-    float fStayTimer = 0;
+	public ParticleSystem eaten;
 
-    //Property for ChilliHealth with auto check wheter its dead
-    public float fChilliHealth
-    {
-        get { return _fChilliHealth; }
-        set
-        {
-            _fChilliHealth = value;
-            if (_fChilliHealth <= 0)
-            {
-                // Disable Object
-                this.gameObject.SetActive(false);
-//                Debug.Log(this.gameObject.name + " should have been disabled");
-            }
-        }
-    }
-
-    //Set defaut value
-    [SerializeField] private float _fChilliHealth = 100;
+	private ParticleSystem particleEaten;
+	//Counts Time for triggerd Damage Script - Every Second Damage should happen
+	private float fStayTimer = 0;
+	//Set defaut value
+	[SerializeField] private float _fChilliHealth = 100;
 
 
-    //Applys the damage
-    private void DoDamage(Collider col)
-    {
+	private void Start() {
+		particleEaten = Instantiate(eaten, transform.position, Quaternion.identity);
+	}
+	//Property for ChilliHealth with auto check wheter its dead
+	public float fChilliHealth {
+		get { return _fChilliHealth; }
+		set {
+			_fChilliHealth = value;
+			if (_fChilliHealth <= 0) {
+				// Disable Object
+				this.gameObject.SetActive(false);
+				if (particleEaten.isPlaying) particleEaten.Stop();
 
-        if (col.gameObject.tag == "Enemy")
-        {
-            //Do Damage ever Second
-            fStayTimer += Time.deltaTime;
-            if (fStayTimer > 1)
-            {
-                //Reset timer - counts only to one second...
-                fStayTimer = 0;
+				//                Debug.Log(this.gameObject.name + " should have been disabled");
+			}
+		}
+	}
 
-                //Get Link to DefaultEnemieSettings for getting damage of enemy
-                //GameObject Enemy = GameObject.Find(col.gameObject.name);
-                DefaultEnemySettings EnemyLink = col.gameObject.GetComponent<DefaultEnemySettings>();
-
-                fChilliHealth -= EnemyLink.fDamage;
-            }
-        }
-
-//        Debug.Log("Remaing health of " + this.gameObject.name + ": " + fChilliHealth);
-    }
+	
 
 
-    //Do Damage evry second while collision
-    private void OnTriggerStay(Collider col)
-    {
-        DoDamage(col);
+	//Applys the damage
+	private void DoDamage(Collider col) {
+		if (!particleEaten.isPlaying) particleEaten.Play();
+		if (col.gameObject.tag == "Enemy") {
+			//Do Damage ever Second
+			fStayTimer += Time.deltaTime;
+			if (fStayTimer > 1) {
+				//Reset timer - counts only to one second...
+				fStayTimer = 0;
 
-    }
+				//Get Link to DefaultEnemieSettings for getting damage of enemy
+				//GameObject Enemy = GameObject.Find(col.gameObject.name);
+				DefaultEnemySettings EnemyLink = col.gameObject.GetComponent<DefaultEnemySettings>();
 
-    //Initialize Timer
-    private void OnTriggerEnter(Collider col)
-    {
-        DoDamage(col);
+				fChilliHealth -= EnemyLink.fDamage;
+			}
+		}
 
-    }
+		//        Debug.Log("Remaing health of " + this.gameObject.name + ": " + fChilliHealth);
+	}
+
+
+	//Do Damage evry second while collision
+	private void OnTriggerStay(Collider col) {
+		DoDamage(col);
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (particleEaten.isPlaying) particleEaten.Stop(); 
+	}
+	//Initialize Timer
+	private void OnTriggerEnter(Collider col) {
+		DoDamage(col);
+	}
 
 }
