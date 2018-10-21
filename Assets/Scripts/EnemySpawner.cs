93fc58629beed0654901bcc97875c0510d6034a7
ALien747 <22078283+ALien747@users.noreply.github.com>
2018-10-21 17:04:12 +0200

    A10

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : Spawner
+{
+
+    public override float SpawnRate()
+    {
+// Change Function to whatever you want 
+        return Mathf.Clamp(5 - Mathf.Log((Time.time) / 120 + 1), 0.5f, 10);
+    }
+}
