bf790b06b3a28e3b4065416915af06c946e28f6c
ALien747 <22078283+ALien747@users.noreply.github.com>
2018-10-21 17:09:28 +0200

    Revert "A10"
    This reverts commit 93fc58629beed0654901bcc97875c0510d6034a7.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,93 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Spawner : MonoBehaviour
-{
-    public List<GameObject> toSpawnTypes;
-    public List<Transform> spawnPoints;
-
-    public int maxAmount = 20;
-
-    [SerializeField, Range(1, 10)]
-    private float spawnRate = 1;
-
-    private float timeSinceLastSpawn = 0;
-    private static Spawner _instance;
-    public static Spawner Instance { get { return _instance; } }
-
-    public bool canSpawn = true;
-
-    private void Awake()
-    {
-        if (_instance != null && _instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            _instance = this;
-        }
-    }
-    void Start()
-    {
-        if (toSpawnTypes.Count == 0) Debug.Log("Types list empty");
-
-        if (spawnPoints.Count == 0) spawnPoints.Add(transform);
-        foreach (var item in toSpawnTypes)
-        {
-            SimplePool.Preload(item, maxAmount);
-        }
-    }
-
-    public virtual float SpawnRate()
-    {
-        return Mathf.Clamp(5 - Mathf.Log((Time.time) / 120 + 1), 0.5f, 10);
-    }
-    private void Update()
-    {
-        spawnRate = SpawnRate();
-        if (timeSinceLastSpawn >= spawnRate && canSpawn)
-        {
-            StartCoroutine("Spawn");
-
-            timeSinceLastSpawn = 0;
-        }
-        timeSinceLastSpawn += Time.deltaTime;
-    }
+public class Spawner : MonoBehaviour {
+	public List<GameObject> enemyTypes;
+	public List<Transform> spawnPoints;
 
 
-    public void Despawn(GameObject gObject)
-    {
-        SimplePool.Despawn(gObject);
-        gObject.transform.position = Spawner.Instance.getRandomSpawnPoint();
-    }
 
 
-    public Vector3 getRandomSpawnPoint()
-    {
-        return spawnPoints.RandomElement<Transform>().position; ;
-    }
+    [SerializeField]
+    public int iSpawnRangeLow = 1;
+    [SerializeField]
+    public int iSpawnRangeHigh = 5;
+
+	public int maxAmount = 20;
+
+	[SerializeField, Range(1, 10)]
+	private float spawnRate = 1;
+
+	private float timeSinceLastSpawn = 0;
+	private static Spawner _instance;
+	public static Spawner Instance { get { return _instance; } }
+
+	public bool canSpawn = true;
+
+	private void Awake() {
+		if (_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+		}
+		else {
+			_instance = this;
+		}
+	}
+	void Start() {
+		if (enemyTypes.Count == 0) Debug.Log("Enemy types list empty");
+
+		if (spawnPoints.Count == 0) spawnPoints.Add(transform);
+		foreach (var item in enemyTypes) {
+			SimplePool.Preload(item, maxAmount);
+		}
+	}
 
 
-    IEnumerator Spawn()
-    {
-        int amount = Random.Range(1, 5);
+	private void Update() {
+		spawnRate = Mathf.Clamp(5 - Mathf.Log((Time.time) / 120 + 1), 0.5f, 10);
+		if (timeSinceLastSpawn >= spawnRate && canSpawn) {
+			StartCoroutine("spawnEnemies");
 
-        for (int i = 0; i < amount; i++)
-        {
-            GameObject temp = SimplePool.Spawn(toSpawnTypes.RandomElement<GameObject>(), getRandomSpawnPoint(), Quaternion.identity);
-            temp.transform.SetParent(gameObject.transform);
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
+			timeSinceLastSpawn = 0;
+		}
+		timeSinceLastSpawn += Time.deltaTime;
+	}
+
+
+	public void Despawn(GameObject gObject) {
+		SimplePool.Despawn(gObject);
+		gObject.transform.position = Spawner.Instance.getRandomSpawnPoint();
+
+	}
+
+
+	public Vector3 getRandomSpawnPoint() {
+		Vector3 pos = spawnPoints.RandomElement<Transform>().position;
+		return pos;
+	}
+
+
+	IEnumerator spawnEnemies() {
+
+
+        
+		int amount = Random.Range(iSpawnRangeLow, iSpawnRangeHigh);
+
+		for (int i = 0; i < amount; i++) {
+			GameObject temp = SimplePool.Spawn(enemyTypes.RandomElement<GameObject>(), getRandomSpawnPoint(), Quaternion.identity);
+			temp.transform.SetParent(gameObject.transform);
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
 }
 
 
-public static class CollectionExtension
-{
+public static class CollectionExtension {
 
-    public static T RandomElement<T>(this IList<T> list)
-    {
-        return list[Random.Range(0, list.Count)];
-    }
+	public static T RandomElement<T>(this IList<T> list) {
+		return list[Random.Range(0, list.Count)];
+	}
+
+	public static T RandomElement<T>(this T[] array) {
+		return array[Random.Range(0, array.Length)];
+	}
 }
