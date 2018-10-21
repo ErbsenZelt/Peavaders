f76eb9b78da74d2baa0c5b3e4f23831719cfb254
ALien747 <22078283+ALien747@users.noreply.github.com>
2018-10-21 18:38:21 +0200

    Working ^^

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,23 +7,28 @@ using UnityEngine;
  * attached to Enemy transform
  * moves towards the Target Position (Vector3) and tries to avoid all objects with the tag AvoidTag
 */
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-	private GameObject Target;
-	[SerializeField] private GameObject[] ToAvoid; //List of all objects that have the AvoidTag as tag. Only updated on Spawn()
+    private GameObject Target;
+    [SerializeField] private GameObject[] ToAvoid; //List of all objects that have the AvoidTag as tag. Only updated on Spawn()
 
-	//assigned in Editor:
-	[SerializeField] private NavMeshAgent NavAgent;
-	[SerializeField] private Transform[] Eyes;
-	[SerializeField] private Rigidbody RB; //RigidBody of this gameobject
-	private Transform EyeTarget;
 
-	[SerializeField] private ParticleSystem dieAnim;
+    
+    AudioSource DeathSound;
+
+    //assigned in Editor:
+    [SerializeField] private NavMeshAgent NavAgent;
+    [SerializeField] private Transform[] Eyes;
+    [SerializeField] private Rigidbody RB; //RigidBody of this gameobject
+    private Transform EyeTarget;
+
+    [SerializeField] private ParticleSystem dieAnim;
 
     public void FixedUpdate()
     {
 
-      if (Target.activeSelf == false)
+        if (Target.activeSelf == false)
         {
             OnEnable();
         }
@@ -33,33 +38,46 @@ public class Enemy : MonoBehaviour {
 
         //point eyes at eyetargetposition
         foreach (Transform Eye in Eyes) Eye.rotation = Quaternion.LookRotation(EyeTarget.position - transform.position + transform.position.y * Vector3.up, Vector3.up);
-	}
+    }
 
 
-	void OnEnable() {
+    void OnEnable()
+    {
 
-		float fShortestDistance;
-		GameObject NearestGameObject;
+        float fShortestDistance;
+        GameObject NearestGameObject;
 
-		//initialize
-		NearestGameObject = GameObject.FindGameObjectWithTag("Target");
-		fShortestDistance = Vector3.Distance(this.gameObject.transform.position, NearestGameObject.transform.position);
+        //initialize
+        NearestGameObject = GameObject.FindGameObjectWithTag("Target");
+        fShortestDistance = Vector3.Distance(this.gameObject.transform.position, NearestGameObject.transform.position);
 
 
-		foreach (GameObject go in GameObject.FindGameObjectsWithTag("Target")) {
-			if (Vector3.Distance(this.gameObject.transform.position, go.transform.position) < fShortestDistance) {
-				fShortestDistance = Vector3.Distance(this.gameObject.transform.position, go.transform.position);
-				NearestGameObject = go;
-			}
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Target"))
+        {
+            if (Vector3.Distance(this.gameObject.transform.position, go.transform.position) < fShortestDistance)
+            {
+                fShortestDistance = Vector3.Distance(this.gameObject.transform.position, go.transform.position);
+                NearestGameObject = go;
+            }
 
-			//Debug.Log(go.name + ": " + Vector3.Distance(this.gameObject.transform.position, go.transform.position));
-		}
+            //Debug.Log(go.name + ": " + Vector3.Distance(this.gameObject.transform.position, go.transform.position));
+        }
 
-		Target = NearestGameObject;
-	}
+        Target = NearestGameObject;
+    }
 
-	public void Die() {
-		Instantiate(dieAnim, gameObject.transform.position, Quaternion.identity);
-		Spawner.Instance.Despawn(gameObject);
-	}
+    private void Start()
+    {
+        DeathSound = GameObject.Find("DeathSound").gameObject.GetComponent<AudioSource>();
+
+    }
+
+    public void Die()
+    {
+        DeathSound.Play();
+
+
+        Instantiate(dieAnim, gameObject.transform.position, Quaternion.identity);
+        Spawner.Instance.Despawn(gameObject);
+    }
 }
